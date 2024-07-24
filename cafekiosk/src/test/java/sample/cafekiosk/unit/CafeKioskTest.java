a6d91e3d63a25c560f76997c36c66d7d24eff0e7
장우진 <dnwls16071@naver.com>
2024-07-24 22:51:32 +0900

    :white_check_mark: 해피 케이스 및 예외 케이스 테스트 코드 작성

--- cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -10,7 +10,7 @@ class CafeKioskTest {
 	@Test
 	void add_manual_test() {
 		CafeKiosk cafeKiosk = new CafeKiosk();
-		cafeKiosk.add(new Americano());
+		//cafeKiosk.add(new Americano(), 1);
 		System.out.println(">>> 담긴 음료 수: " + cafeKiosk.getBeverages().size());
 		System.out.println(">>> 담긴 음료: " + cafeKiosk.getBeverages().get(0).getName());
 	}
@@ -18,7 +18,7 @@ class CafeKioskTest {
 	@Test
 	void add() {
 		CafeKiosk cafeKiosk = new CafeKiosk();
-		cafeKiosk.add(new Americano());
+		//cafeKiosk.add(new Americano(), 1);
 
 		assertThat(cafeKiosk.getBeverages()).hasSize(1);
 		assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
@@ -29,7 +29,7 @@ class CafeKioskTest {
 		CafeKiosk cafeKiosk = new CafeKiosk();
 		Americano americano = new Americano();
 
-		cafeKiosk.add(americano);
+		cafeKiosk.add(americano, 1);
 		assertThat(cafeKiosk.getBeverages()).hasSize(1);
 
 		cafeKiosk.remove(americano);
@@ -42,11 +42,40 @@ class CafeKioskTest {
 		Americano americano = new Americano();
 		Latte latte = new Latte();
 
-		cafeKiosk.add(americano);
-		cafeKiosk.add(latte);
+		//cafeKiosk.add(americano, 1);
+		//cafeKiosk.add(latte, 1);
 		assertThat(cafeKiosk.getBeverages()).hasSize(2);
 
 		cafeKiosk.clear();
 		assertThat(cafeKiosk.getBeverages()).isEmpty();
+	}
+
+	@Test
+	void addSeveralBeverage() {
+		CafeKiosk cafeKiosk = new CafeKiosk();
+		Americano americano = new Americano();
+
+		// 해피 케이스
+		cafeKiosk.add(americano, 2);
+
+		assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+		assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+	}
+
+	@Test
+	void addZeroBeverages() {
+		CafeKiosk cafeKiosk = new CafeKiosk();
+		Americano americano = new Americano();
+
+		/*
+		assertThrows(IllegalStateException.class, () -> {
+			cafeKiosk.add(americano, 0);
+		});
+		 */
+
+		// 예외 케이스
+		assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+				.isInstanceOf(IllegalStateException.class)
+				.hasMessage("음료는 최소 한 잔 이상 주문해야합니다!");
 	}
 }
